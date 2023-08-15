47f7e7c3832da3c17e5bb9ef38d169f1ad60b125
тaкudzwa кuснегега <takuchbtk@gmail.com>
2023-08-15 08:39:31 +0200

    Protected Data

--- AdvancedSecurity/Program.cs
+++ AdvancedSecurity/Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.AspNetCore.DataProtection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+
+
+
+Console.WriteLine("=====================================Protecting Data===================================");
+
+// 1. It all begins by creating an instance of DataProtectionProvider
+var provider = DataProtectionProvider.Create("AdvancedSecurityDataProtection");
+
+// 2. Create a Protector
+var protector = provider.CreateProtector("DataProtection");
+
+// 3. Protect something
+var sensitivePlainText = "Hello world!";
+var protectedText = protector.Protect(sensitivePlainText);
+
+
+Console.WriteLine($"Protected text: {protectedText}");
+
+// unprotect the protected text
+var unProtectedText = protector.Unprotect(protectedText);
+Console.WriteLine($"Unprotected text: {unProtectedText}");
